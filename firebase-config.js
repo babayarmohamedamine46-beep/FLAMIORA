b72b0f52f439e944580c94ffd2dda484b065d4e5
babayarmohamedamine46-beep <babayarmohamedamine46@gmail.com>
2026-08-28 04:26:29 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC7YHA1t8LTW3Ob1oQnRnA5jx78iHuaQQ0",
+  authDomain: "flamiora-f0062.firebaseapp.com",
+  projectId: "flamiora-f0062",
+  storageBucket: "flamiora-f0062.firebasestorage.app",
+  messagingSenderId: "995632463964",
+  appId: "1:995632463964:web:76c04597312e738acea699",
+  measurementId: "G-TF4TVKY6CY"
 };
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
